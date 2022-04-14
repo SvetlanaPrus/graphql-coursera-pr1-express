09c99652bb24cc9c59cf8231c38249fce1aa9e5c
Svetlana Prus <swetlana.prus@gmail.com>
2022-04-14 18:55:18 +0200

    2.Set up an access point and serve the list of merchandise

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require('express');
 const cors = require('cors');
 
 const { response } = require('express');
+
+const { graphqlHTTP } = require('express-graphql');
+const gql = require('graphql-tag');
+const { buildASTSchema } = require('graphql');
 const dbJson = require('./items-db.json');
 
 const app = express();
@@ -11,6 +15,31 @@ app.use(cors({ origin: '*', credentials: true }));
 
 // serve static file
 app.use('/images', express.static('public'));
+
+// schema
+const schema = buildASTSchema(gql(`
+  type Query {
+      hello: String!
+      merchandises: [ Merchandise ]
+  }
+
+  type Merchandise {
+      id: ID
+      name: String
+      description: String
+      lastBid: Float
+      lastBidUser: String
+      imageUrl: String
+  }
+`));
+
+// resolver
+const rootValue = {
+  hello: () => 'Hello World!',
+  merchandises: () => dbJson,
+};
+
+app.use('/', graphqlHTTP({ schema, rootValue }));
 
 app.listen(
   3000,
